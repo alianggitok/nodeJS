0c476f8aa96cd0fed68eda6301c51848edaf3cf5
alianggitok <alianggmok@gmail.com>
2014-06-18 09:57:29 +0800

    sdfasdf

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+function start() { 
+	function onRequest(request, response) {
+		console.log('Request received.');
+		response.writeHead(200, { 'Content-Type': 'text/html' });
+		response.write('Hello, world!');
+		response.end();
+	};
+
+	var server=http.createServer(onRequest);
+
+	server.listen(process.env.PORT || 8080);
+
+	console.log('Server has started.');
+};
+
+exports.start = start;
